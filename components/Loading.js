e5d935b57230f67347ac63188eb4369744a51b7d
Adeoluwa Apata <40585277+Adeoluwa0@users.noreply.github.com>
2021-07-20 01:58:08 +0100

    Update Loading.js

--- components/Loading.js
+++ components/Loading.js
@@ -1,6 +1,6 @@
 
 import { Circle} from "better-react-spinkit";
-import Image from 'next/image'
+import Image from 'next/image';
 
 function Loading() {
     return (
@@ -9,8 +9,7 @@ function Loading() {
                 <Image src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" 
                 alt=""
                 style ={{marginBottom:10}}  
-                height={200}       
-              
+                height={200}
                 />
                 <Circle color="#3CBC28" size={60} />
             </div>
